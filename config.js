54cc3e00a979b81aa735b42b463d99b3ce03d696
Billy Hoang <billy-hoang@hotmail.com>
2017-09-10 01:27:59 -0700

    Revert "Functioning twitter bot that tweets every 5 seconds"
    This reverts commit fcc1a46a6ef4294ab709678101742b5b4f7fd2ca.

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key:         'vyndj5qsn5kqaqYJ4Stdqcx7z',
-  consumer_secret:      'blwD2jwk2zYAOYC3czzUIi9a6S1GSzvgWIsqzKAo7weaOkOP2z',
-  access_token:         '906781976807456768-U5LCOS33VB55zfeSyImENDZa8KtxetO',
-  access_token_secret:  'n8BQBuPh1brgNX0t7WNF3gT58c11UYpGKtsUdlQOtd5mU',  
-};
